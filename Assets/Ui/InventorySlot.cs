8bb552fc7cf3010d6a15dcb6ea35de4818c53863
Neferius Leviathanis <neferius.leviathanis@gmail.com>
2019-04-27 15:57:54 +0200

    Added items

---
+++ Assets/Ui/InventorySlot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour {
+    private Button itemButton;
+    private Image itemImage;
+    private Item item;
+
+    private void Start() {
+        itemButton = transform.GetChild(0).GetComponent<Button>();
+        itemImage = itemButton.transform.GetChild(0).GetComponent<Image>();
+    }
+    
+    public bool IsEmpty() {
+        return item == null;
+    }
+
+    public Item GetItem() {
+        return item;
+    }
+
+    public void SetItem(Item item) {
+        this.item = item;
+
+        itemButton.interactable = true;
+        itemImage.enabled = true;
+        itemImage.sprite = item.icon;
+    }
+
+    public void Empty() {
+        itemButton.interactable = false;
+        itemImage.enabled = false;
+        this.item = null;
+    }
+}
--- Assets/Ui/ItemRegistry.cs
+++ Assets/Ui/ItemRegistry.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemRegistry : MonoBehaviour {
+    private static ItemRegistry INSTANCE = null;
+
+    public static ItemRegistry GetInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = (ItemRegistry) FindObjectOfType(typeof(ItemRegistry));
+        }
+
+        return INSTANCE;
+    }
+    
     [SerializeField]
     private Item[] items;
+
+    public Item GetItem(int id) {
+        if (id < 0 || id > items.Length) {
+            return null;
+        }
+        
+        return items[id];
+    }
 }
