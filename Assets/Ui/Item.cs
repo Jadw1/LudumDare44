111244c9b321a3b3cb82c7fef4723f142170bc0d
Neferius Leviathanis <neferius.leviathanis@gmail.com>
2019-04-27 15:02:08 +0200

    Started work on items

---
+++ Assets/Ui/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public enum ItemType {
+    BAUBLE,
+    WEAPON,
+    SHIELD,
+    ARMOR,
+    HELMET,
+    RING
+}
+
+[System.Serializable]
+public class Item {
+    public Sprite icon;
+    public string name;
+    public string description;
+    public int price;
+    public ItemType type;
+}
