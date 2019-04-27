986b116155d9079b880e08feeffec1cfb0b4884d
Neferius Leviathanis <neferius.leviathanis@gmail.com>
2019-04-27 07:57:44 +0200

    Refactored code

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,35 +4,77 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class GameMaster : MonoBehaviour {
-    private static GameMaster INSTANCE = null;
 
-    public static GameMaster getGameMaster() {
-        if (INSTANCE == null) {
-            INSTANCE = (GameMaster) FindObjectOfType(typeof(GameMaster));
-        }
+    [SerializeField]
+    private Tile highlightTile = null;
 
-        return INSTANCE;
-    }
+    private Color overlayColorNormal = new Color(1.0f, 1.0f, 1.0f, 0.2f);
+    private Color overlayColorHighlight = new Color(1.0f, 1.0f, 0.0f, 0.2f);
 
-    private Tilemap obstacles;
-    private Tilemap floor;
+    private Vector3Int previousOverlayHighlight = new Vector3Int();
+
+    private Tilemap top;
+    private Tilemap middle;
+    private Tilemap bottom;
+    private Tilemap overlay;
+
+    private TileEntity player;
+
+    private TileEntity[] enemies;
 
     private void Start() {
         Grid grid = (Grid) FindObjectOfType(typeof(Grid));
 
-        obstacles = grid.transform.Find("Middle").GetComponent<Tilemap>();
-        floor = grid.transform.Find("Bottom").GetComponent<Tilemap>();
+        top = grid.transform.Find("Top").GetComponent<Tilemap>();
+        middle = grid.transform.Find("Middle").GetComponent<Tilemap>();
+        bottom = grid.transform.Find("Bottom").GetComponent<Tilemap>();
+        overlay = grid.transform.Find("Overlay").GetComponent<Tilemap>();
+
+        player = GameObject.FindWithTag("Player").GetComponent<TileEntity>();
+        
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Enemy");
+        enemies = new TileEntity[objects.Length];
+
+        for (int i = 0; i < enemies.Length; i++) {
+            enemies[i] = objects[i].GetComponent<TileEntity>();
+        }
+
+        RebuildOverlay();
     }
 
-    public bool canPass(Vector3Int v) {
-        return !obstacles.HasTile(v) && floor.HasTile(v);
+    private void Update() {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int gridPos = overlay.WorldToCell(mousePos);
+        
+        if (gridPos != previousOverlayHighlight) {
+            if (overlay.HasTile(gridPos)) {
+                overlay.SetColor(gridPos, overlayColorHighlight);
+            }
+            
+            if (overlay.HasTile(previousOverlayHighlight)) {
+                overlay.SetColor(previousOverlayHighlight, overlayColorNormal);
+            }
+
+            previousOverlayHighlight = gridPos;
+        }
+
+        if (Input.GetMouseButtonDown(0) && overlay.HasTile(gridPos)) {
+            player.MoveTo(gridPos.x, gridPos.y);
+            RebuildOverlay();
+        }
     }
 
-    public bool move(Vector3Int v) {
-        if (!canPass(v)) return false;
-
-        // STUB
-
-        return true;
+    private void RebuildOverlay() {
+        overlay.ClearAllTiles();
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                Vector3Int pos = player.GetPos() + new Vector3Int(x - 1, y - 1, 0);
+                if ((x != 1 || y != 1) && bottom.HasTile(pos)) {
+                    overlay.SetTile(pos, highlightTile);
+                    overlay.SetTileFlags(pos, TileFlags.LockTransform);
+                    overlay.SetColor(pos, overlayColorNormal);
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/TileEntity.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileEntity : MonoBehaviour {
+    private int posX, posY;
+    private bool isMoving = false;
+    private Vector3 targetPosition = new Vector3();
+    private Vector3 dummy = new Vector3();
+
+    private void Start() {
+        Tilemap grid = (Tilemap) FindObjectOfType(typeof(Tilemap));
+        Vector3Int pos = grid.WorldToCell(this.transform.position);
+
+        posX = pos.x;
+        posY = pos.y;
+
+        targetPosition = pos;
+    }
+    
+    public void Move(int x, int y) {
+        posX += x;
+        posY += y;
+
+        targetPosition = new Vector3(x, y, 0);
+        isMoving = true;
+    }
+
+    public void MoveTo(int x, int y) {
+        posX = x;
+        posY = y;
+
+        targetPosition = new Vector3(x, y, 0);
+        isMoving = true;
+    }
+
+    public Vector3Int GetPos() {
+        return new Vector3Int(this.posX, this.posY, 0);
+    }
+
+    private void Update() {
+        if (isMoving) {
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref dummy, 0.05f);
+            
+            if (Mathf.Abs((transform.position - targetPosition).magnitude) <= 0.01f) {
+                isMoving = false;
+                transform.position = targetPosition;
+            }
+        }
+    }
+}
