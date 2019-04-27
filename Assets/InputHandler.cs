d0227e7b21a80028336eead67e25cdf2b4a2d432
Jadw1 <jadw1_1@o2.pl>
2019-04-27 14:46:26 +0200

    rebuilding managers and added extra unwanted feature

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OverlayManager))]
+public class InputHandler : MonoBehaviour {
+
+    private GameMaster gameMaster;
+
+    private TilePos previousHighlight;
+
+    private void Start() {
+        gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+        previousHighlight = new TilePos();
+    }
+
+    private void Update() {
+        TilemapManager tilemap = TilemapManager.GetInstance();
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        TilePos gridPos = tilemap.WorldToCell(mousePos);
+
+        OverlayManager overlay = OverlayManager.GetInstance();
+        if(overlay == null)
+            Debug.Log("XD");
+
+        if(gridPos != previousHighlight) {
+            if(tilemap.HasOverlay(gridPos)) {
+                overlay.SetTileColorNormal(gridPos);
+            }
+
+            if(tilemap.HasOverlay(previousHighlight)) {
+                overlay.SetTileColorHover(previousHighlight);
+            }
+
+            previousHighlight = gridPos;
+        }
+
+        if(Input.GetMouseButtonDown(0) && tilemap.HasOverlay(gridPos)) {
+            gameMaster.PerformAction(gridPos);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TileEntity))]
+public class Player : MonoBehaviour, IAbility {
+
+    private TileEntity tileEntity;
+
+    private void Start() {
+        tileEntity = transform.GetComponent<TileEntity>();
+    }
+
+    public TilePos GetPos() {
+        return tileEntity.GetPos();
+    }
+
+    #region IAbility
+    public void Execute(TilePos pos) {
+        tileEntity.Move(pos);
+    }
+
+    public TilePos[] GetValidTiles() {
+        List<TilePos> possibilities = new List<TilePos>();
+        for(int x = -1; x < 2; x++) {
+            for(int y = -1; y < 2; y++) {
+                if(x != 0 || y != 0) {
+                    TilePos pos = new TilePos(x, y);
+                    possibilities.Add(pos);
+                }
+            }
+        }
+
+        possibilities.Add(new TilePos(0, 2));
+        possibilities.Add(new TilePos(0, -2));
+        possibilities.Add(new TilePos(2, 0));
+        possibilities.Add(new TilePos(-2, 0));
+
+        return possibilities.ToArray();
+    }
+    #endregion
+}
